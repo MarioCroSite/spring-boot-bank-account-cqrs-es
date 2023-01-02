5d90191530237376f5330b44b9a01a28ec65afce
Mario <crositecom@gmail.com>
2023-01-02 14:54:52 +0100

    Added Account Event Store

---
+++ account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventSourcingHandler.java
@@ -0,0 +1,35 @@
+package com.techbank.account.cmd.infrastructure;
+
+import com.techbank.account.cmd.domain.AccountAggregate;
+import com.techbank.cqrs.core.domain.AggregateRoot;
+import com.techbank.cqrs.core.events.BaseEvent;
+import com.techbank.cqrs.core.handlers.EventSourcingHandler;
+import com.techbank.cqrs.core.infrastructure.EventStore;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+
+@Service
+public class AccountEventSourcingHandler implements EventSourcingHandler<AccountAggregate> {
+    private EventStore eventStore;
+
+    @Override
+    public void save(AggregateRoot aggregateRoot) {
+        eventStore.saveEvents(aggregateRoot.getId(), aggregateRoot.getUncommittedChanges(), aggregateRoot.getVersion());
+        aggregateRoot.markChangesAsCommitted();
+    }
+
+    @Override
+    public AccountAggregate getById(String id) {
+        var aggregate = new AccountAggregate();
+        var events = eventStore.getEvent(id);
+        if(events != null && !events.isEmpty()) {
+            aggregate.replayEvents(events);
+            var latestVersion = events.stream().map(BaseEvent::getVersion).max(Comparator.naturalOrder());
+            aggregate.setVersion(latestVersion.get());
+        }
+
+        return aggregate;
+    }
+
+}
