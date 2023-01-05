b749175d2c1f03df769241dcfd1728340cb80cb3
Mario <crositecom@gmail.com>
2023-01-05 16:04:12 +0100

    Added Query classes

---
+++ account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountWithBalanceQuery.java
@@ -0,0 +1,10 @@
+package com.techbank.account.query.api.queries;
+
+import com.techbank.account.query.api.dto.EqualityType;
+import com.techbank.cqrs.core.queries.BaseQuery;
+
+public class FindAccountWithBalanceQuery extends BaseQuery {
+    private EqualityType equalityType;
+    private double balance;
+
+}
